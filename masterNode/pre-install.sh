f3db80e17f956301dd90205ee8b7ea6daa0abe89
Édouard Dupont <57630316+th3m1s-42@users.noreply.github.com>
2021-08-12 18:15:24 +0200

    Update and rename install.sh to pre-install.sh

---
+++ masterNode/pre-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+REPOSITORY="https://github.com/th3m1s-42/deepThough/tree/main/masterNode/config-files/"
+TEMPLATE_DIR="/tmp/GitHub/masterNode"
+
+# upgrade Raspberry Pi OS
+#apt-get update
+#apt-get upgrade
+
+# install some usefull packages
+
+
+# copy config templates from repository
+if [[ ! -d $TEMPLATE_DIR ]];
+  then mkdir -p $TEMPLATE_DIR
+fi
+#git clone $REPOSITORY $TEMPLATE_DIR
